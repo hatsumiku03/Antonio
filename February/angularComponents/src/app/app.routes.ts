b9308c935280414e744b25f5c1672f8924c01cad
Joselito <josalbarr@alu.edu.gva.es>
2025-02-12 13:11:43 +0100

    trying components

--- February/angularComponents/src/app/app.routes.ts
+++ February/angularComponents/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HoliComponent } from './holi/holi.component';
+import { ComponentsComponent } from './components/components.component';
 
 export const routes: Routes = [
     { path: 'holi', component: HoliComponent },
+    { path: 'components', component: ComponentsComponent },
 ];
--- February/angularComponents/src/app/components/components.component.ts
+++ February/angularComponents/src/app/components/components.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-components',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './components.component.html',
   styleUrl: './components.component.css'
 })
